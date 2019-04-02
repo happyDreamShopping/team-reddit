e82d8a876dfcbc9577f918d9f35854bdcb81c045
occidere <occidere@naver.com>
2019-04-02 12:41:31 +0900

    Initial commit

---
+++ src/main/java/study/shopelk/teamreddit/TeamRedditApplication.java
@@ -0,0 +1,13 @@
+package study.shopelk.teamreddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamRedditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamRedditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/shopelk/teamreddit/TeamRedditApplicationTests.java
@@ -0,0 +1,16 @@
+package study.shopelk.teamreddit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TeamRedditApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
