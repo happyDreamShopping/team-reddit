292e94a9d21fea5928f41966f2fee700b777ce81
occidere <occidere@naver.com>
2019-04-03 11:13:46 +0900

    [WEB-API] 컨트롤러 기초 세팅

--- src/main/java/study/shopelk/teamreddit/TeamRedditApplication.java
+++ src/main/java/study/shopelk/teamreddit/TeamRedditApplication.java
@@ -3,11 +3,17 @@ package study.shopelk.teamreddit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author occidere
+ * @blog: https://blog.naver.com/occidere
+ * @github: https://github.com/occidere
+ * @since 2019-04-03
+ */
 @SpringBootApplication
 public class TeamRedditApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TeamRedditApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TeamRedditApplication.class, args);
+    }
 
 }
---
+++ src/main/java/study/shopelk/teamreddit/controller/TeamRedditController.java
@@ -0,0 +1,39 @@
+package study.shopelk.teamreddit.controller;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author occidere
+ * @blog: https://blog.naver.com/occidere
+ * @github: https://github.com/occidere
+ * @since 2019-04-03
+ */
+@Slf4j
+@RestController
+public class TeamRedditController {
+
+    @GetMapping("/health")
+    public String health() {
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);
+        log.info("Current Timestamp: {}", timestamp);
+        return timestamp;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/search", method = RequestMethod.POST)
+    public String search(@RequestParam("author") String author) {
+        log.info("Author: {}", author);
+
+        // search
+
+        return "Search reseult of " + author;
+    }
+}
